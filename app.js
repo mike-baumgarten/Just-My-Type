7bc3e32344b3d2c2268584ef923e71f469039dbe
mike-baumgarten <32228925+mike-baumgarten@users.noreply.github.com>
2020-06-03 21:27:48 -0600

    built functions to make code look a bit cleaner

--- app.js
+++ app.js
@@ -1,155 +1,196 @@
 // variables
-let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+let oldsentences = [
+  "ten ate neite ate nee enet ite ate inet ent eate ",
+  "Too ato too nOt enot one totA not anot tOO aNot ",
+  "oat itain oat tain nate eate tea anne inant nean ",
+  "itant eate anot eat nato inate eat anot tain eat ",
+  "nee ene ate ite tent tiet ent ine ene ete ene ate",
+];
+let sentences = [
+  "ten ate neite ate nee enet ite ate inet ent eate ",
+  "Too ato too nOt enot one totA not anot tOO aNot",
+];
 let currentSentence = sentences[0];
 let totalSentences = 0;
 let letterIndex = 0;
 let mistakeCount = 0;
-
+let timerStarted = null;
+let totalWords = 54;
+let startTime, endTime, totalTime;
 // Functions
 
 // adds span with green checkmark to #feedback
 function correctLetter() {
-
-    let $addSpan = $('<span>').attr('class', 'glyphicon glyphicon-ok')
-    $('#feedback').append($addSpan)
-};
+  let $addSpan = $("<span>").attr("class", "glyphicon glyphicon-ok");
+  $("#feedback").append($addSpan);
+}
 
 // increases mistakes and adds span with red X to #feedback
 function inCorrectLetter() {
-
-    mistakeCount++
-    let $addSpan = $('<span>').attr('class', 'glyphicon glyphicon-remove')
-    $('#feedback').append($addSpan)
+  mistakeCount++;
+  let $addSpan = $("<span>").attr("class", "glyphicon glyphicon-remove");
+  $("#feedback").append($addSpan);
 }
 
 // set yellow block back to initial state
 function resetYellowBlock() {
-    $('#yellow-block').animate({ left: `15px` }, { duration: 125, easing: `linear` });
+  $("#yellow-block").animate(
+    { left: `15px` },
+    { duration: 125, easing: `linear` }
+  );
 }
 
 // set target letter
 function targetLetter() {
-    $('#target-letter').text(currentSentence[letterIndex])
+  $("#target-letter").text(currentSentence[letterIndex]);
 }
 
 // next sentence
 function nextSentence() {
-    // empties div with id feedback
-    $('#feedback').empty()
+  // empties div with id feedback
+  $("#feedback").empty();
 
-    // reset letterIndex
-    letterIndex = 0
+  // reset letterIndex
+  letterIndex = 0;
 
-    // increases totalSentence by 1
-    totalSentences++
+  // increases totalSentence by 1
+  totalSentences++;
 
-    // resets div with ID yellow-block
-    resetYellowBlock();
+  // resets div with ID yellow-block
+  resetYellowBlock();
 
-    // sets currentSentence
-    currentSentence = sentences[totalSentences]
-    $('#sentence').text(currentSentence)
+  // sets currentSentence
+  currentSentence = sentences[totalSentences];
+  $("#sentence").text(currentSentence);
 }
 
-// game end
-function lastSentence() {
-    // empties divs with id feedback and target-letter
-    $('#feedback, #target-letter').empty()
-
-    // resets div with ID yellow-block
-    resetYellowBlock();
-
-    $('#feedback').text(mistakeCount)
+// game completed
+function gameCompleted() {
+  stop();
+  $(document).off();
+  // empties divs with id feedback, target-letter, sentence
+  $("#feedback, #target-letter, #sentence").empty();
+  $(".highlight").removeClass("highlight");
+  // resets div with ID yellow-block
+  resetYellowBlock();
+  $("#yellow-block").hide();
+  let wpm = totalWords / totalTime - 2 * mistakeCount;
+  $("#feedback").text(
+    "Congratulations, you typed " + wpm + " words per minute"
+  );
 }
+
+// start timer
+function start() {
+  startTime = new Date();
+}
+
+// stop timer
+function stop() {
+  endTime = new Date();
+  let timeDiff = endTime - startTime; //in ms
+  // in seconds
+  timeDiff /= 1000;
+  totalTime = timeDiff / 60;
+}
+
 // sets div with id sentence text to current sentence
-$('#sentence').text(currentSentence)
+$("#sentence").text(currentSentence);
 
 //Sets starting target letter
 targetLetter();
 
 // Hides Upper Keyboard Container
-$('#keyboard-upper-container').hide();
+$("#keyboard-upper-container").hide();
 
 // Function that looks for the document to hold the shift key (value 16) down and changes the visibility of the keyboard
 // When the key is released it toggle the visibility back to normal
-$(document).keydown(function (e) {
+$(document)
+  .keydown(function (e) {
     if (e.which === 16) {
-        // Had to use show or hide here instead of toggle cause it was constantly toggling the keyboards while the shift key was held down
-        $('#keyboard-upper-container').show()
-        $('#keyboard-lower-container').hide()
+      // Had to use show or hide here instead of toggle cause it was constantly toggling the keyboards while the shift key was held down
+      $("#keyboard-upper-container").show();
+      $("#keyboard-lower-container").hide();
     }
-})
-    .keyup(function (e) {
-        if (e.which === 16) {
-            $('#keyboard-upper-container, #keyboard-lower-container').toggle()
-        }
-        $('.highlight').toggleClass("highlight")
-
-    })
+  })
+  .keyup(function (e) {
+    if (e.which === 16) {
+      $("#keyboard-upper-container, #keyboard-lower-container").toggle();
+    }
+    $(".highlight").toggleClass("highlight");
+  });
 
 // uses jquery ui to highlight keys when pressed
 $(document).keypress(function (e) {
+  // checks if timer has been started
+  if (timerStarted === null) {
+    start();
+    timerStarted = true;
+  }
 
-    let keyID = '#' + e.which;
+  let keyID = "#" + e.which;
 
-    $(keyID).toggleClass("highlight")
+  $(keyID).toggleClass("highlight");
 
-    // Pulls charCode for current letter in sentences
-    let asciiCurrentLetter = currentSentence.charCodeAt(letterIndex)
+  // Pulls charCode for current letter in sentences
+  let asciiCurrentLetter = currentSentence.charCodeAt(letterIndex);
 
-    // moves yellow block right on keypress
-    $('#yellow-block').animate({ left: '+=17.5px' }, { duration: 125, easing: 'linear' });
+  // moves yellow block right on keypress
+  $("#yellow-block").animate(
+    { left: "+=17.5px" },
+    { duration: 125, easing: "linear" }
+  );
 
-    // if statement comparing totalSentences to sentence length.  This allows us to control what happens when we hit the last sentence
-    if (totalSentences < sentences.length - 1) {
-        // if statement comparing letterIndex to current sentence length.  This allows us to control what happens when we hit the last letter of a sentence
-        if (letterIndex < currentSentence.length - 1) {
-            // checks to see if ID of key pressed = ascii of current letter  
-            if (e.which === asciiCurrentLetter) {
-                // if = calls correct letter
-                correctLetter()
-
-            } else {
-                // if not calls inCorrectLetter
-                inCorrectLetter()
-            }
-            letterIndex++
-        } else {
-            // if last letter of sentence checks if the letter is correct
-            if (e.which === asciiCurrentLetter) {
-                correctLetter()
-            } else {
-                inCorrectLetter()
-            }
-
-            // next sentence
-            nextSentence()
-        }
+  // if statement comparing totalSentences to sentence length.  This allows us to control what happens when we hit the last sentence
+  if (totalSentences < sentences.length - 1) {
+    // if statement comparing letterIndex to current sentence length.  This allows us to control what happens when we hit the last letter of a sentence
+    if (letterIndex < currentSentence.length - 1) {
+      // checks to see if ID of key pressed = ascii of current letter
+      if (e.which === asciiCurrentLetter) {
+        // if = calls correct letter
+        correctLetter();
+      } else {
+        // if not calls inCorrectLetter
+        inCorrectLetter();
+      }
+      letterIndex++;
+      targetLetter();
     } else {
-        // if statement comparing letterIndex to current sentence length.  This allows us to control what happens when we hit the last letter of a sentence
-        if (letterIndex < currentSentence.length - 1) {
-            // checks to see if ID of key pressed = ascii of current letter  
-            if (e.which === asciiCurrentLetter) {
-                // if equal calls correctLetter
-                correctLetter();
+      // if last letter of sentence checks if the letter is correct
+      if (e.which === asciiCurrentLetter) {
+        correctLetter();
+      } else {
+        inCorrectLetter();
+      }
 
-            } else {
-                // if not calls inCorrectLetter
-                inCorrectLetter();
-            };
-            letterIndex++;
-        } else {
-            // if last letter of sentence checks if the letter is correct
-            if (e.which === asciiCurrentLetter) {
-                correctLetter();
-            } else {
-                inCorrectLetter();
-            };
-            lastSentence()
-
-        }
+      // next sentence
+      nextSentence();
+      targetLetter();
     }
-
-    targetLetter()
+  } else if (totalSentences === sentences.length - 1) {
+    // if statement comparing letterIndex to current sentence length.  This allows us to control what happens when we hit the last letter of a sentence
+    if (letterIndex < currentSentence.length - 1) {
+      // checks to see if ID of key pressed = ascii of current letter
+      if (e.which === asciiCurrentLetter) {
+        // if equal calls correctLetter
+        correctLetter();
+      } else {
+        // if not calls inCorrectLetter
+        inCorrectLetter();
+      }
+      letterIndex++;
+      targetLetter();
+    } else {
+      // if last letter of sentence checks if the letter is correct
+      if (e.which === asciiCurrentLetter) {
+        correctLetter();
+      } else {
+        inCorrectLetter();
+      }
+      totalSentences++;
+      gameCompleted();
+    }
+  } else {
+    $(".highlight").toggleClass("highlight");
+  }
 });
-
